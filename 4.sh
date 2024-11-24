67787116b7d9879ff565b47494b56628a9a1d8e2
mucego <96732812+mucego@users.noreply.github.com>
2024-11-24 04:02:18 -0300

    Add files via upload

---
+++ 4.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+cd $(dirname "$0")
+
+echo 'Iniciando processo 13 com range 46cc0750000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 46cc0750000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 14 com range 43d45340000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 43d45340000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 15 com range 4522d9c0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4522d9c0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 16 com range 4ae0bdb0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4ae0bdb0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 17 com range 4856a0d0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4856a0d0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 18 com range 46c9add0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 46c9add0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 19 com range 49e07e90000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 49e07e90000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 20 com range 45b43a80000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 45b43a80000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 21 com range 4e964f40000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4e964f40000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 22 com range 4d3d7c80000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4d3d7c80000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 23 com range 4aae8a10000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4aae8a10000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 24 com range 4ef1bed0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4ef1bed0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 25 com range 4b593b00000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4b593b00000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 26 com range 4d0cc9f0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4d0cc9f0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 27 com range 4d30a7e0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4d30a7e0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 28 com range 4f8c5080000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4f8c5080000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 29 com range 4cd74900000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4cd74900000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 30 com range 4ec31820000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4ec31820000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 31 com range 421cb020000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 421cb020000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 32 com range 4fe30310000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4fe30310000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 33 com range 422dec60000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 422dec60000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 34 com range 454aa1e0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 454aa1e0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 35 com range 4fa3eaf0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4fa3eaf0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 36 com range 48c013f0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 48c013f0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 37 com range 49a61880000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 49a61880000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 38 com range 4a8fae70000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4a8fae70000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 39 com range 42d37d30000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 42d37d30000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 40 com range 4c927d10000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4c927d10000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 41 com range 4c2b6720000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4c2b6720000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 42 com range 478fd940000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 478fd940000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 43 com range 4a8f1f80000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4a8f1f80000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 44 com range 4b8e4330000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4b8e4330000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 45 com range 4aa556a0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4aa556a0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 46 com range 4b8c50f0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4b8c50f0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 47 com range 4ce1e210000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4ce1e210000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 48 com range 4fd78e90000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4fd78e90000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 49 com range 4d64a0d0000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4d64a0d0000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
+echo 'Iniciando processo 50 com range 4ea29920000000000:+ffffffffff'
+timeout 540s ./vanitysearch -stop -t 0 -gpu -gpuId 0 -o out.txt --keyspace 4ea29920000000000:+ffffffffff 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 &
+sleep 540
+pkill -f vanitysearch
+
