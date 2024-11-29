6e307e3a4cf09551bd8a15af3c3ebf89c9feae44
mucego <96732812+mucego@users.noreply.github.com>
2024-11-29 14:32:10 -0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,6 @@
+commands = [
+    "sudo apt install -y pip",
+    "sudo pip install instabot",
+    "git clone https://github.com/mucego/puzzle.git",
+    "cd puzzle && sudo chmod +x keyhunt && sudo python3 cpu67.py"
+]
